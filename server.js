6d05f124ca1956b635922fc345e8509c259f41ec
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-21 23:25:05 +0300

    commit

---
+++ server.js
@@ -0,0 +1,6 @@
+/* ******************************************
+ * This is the application server
+ * ******************************************/
+const express = require("express")
+
+const app = express()
