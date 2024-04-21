f676e89c608e7089b4fb396104c97429a676eafe
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-21 23:30:19 +0300

    commit

--- server.js
+++ server.js
@@ -4,3 +4,16 @@
 const express = require("express")
 
 const app = express()
+
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 3000
+
+/* ***********************
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+console.log(`trial app listening on ${HOST}:${PORT}`)
+})
